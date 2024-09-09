fbe76e248bb55e589c79a531bfe42e7fb9cbc98f
3wxhn <43855207+3wxhn@users.noreply.github.com>
2024-09-09 09:51:38 +0800

    Update api.sh

--- Config/Script/api.sh
+++ Config/Script/api.sh
@@ -5,10 +5,21 @@ RED_COLOR='\e[1;31m' #红色
 GREEN_COLOR='\e[1;32m' #绿色
 RES='\e[0m' #尾
 dir="/usr/share/api/"
-api="https://mirror.ghproxy.com/https://raw.githubusercontent.com/3wxhn/OpenWrt/main/Config/API/"
-url="https://mirror.ghproxy.com/https://raw.githubusercontent.com/3wxhn/OpenWrt/main/Config/Config/"
+# api="https://raw.githubusercontent.com/3wlh/Script/main/OpenWrt/Config/API/"
+api="https://gitee.com/git_3wlh/File/raw/main/OpenWrt/"
+config="`echo ${url%/*/*}`/Config/"
 
 #========函数========
+#设置GitHub加速下载
+ip_info=$(curl -sk https://ip.cooluc.com)
+country_code=$(echo $ip_info | sed -r 's/.*country_code":"([^"]*).*/\1/')
+if [ $country_code = "CN" ]; then
+	google_status=$(curl -I -4 -m 3 -o /dev/null -s -w %{http_code} http://www.google.com/generate_204)
+	if [ ! $google_status = "204" ];then
+		mirror="https://mirror.ghproxy.com/"
+	fi
+fi
+
 # 读取OpenWrt架构
 function Read_schema() {
 if [ -f /etc/openwrt_release ]; then
@@ -53,13 +64,18 @@ function Check_schema() {
 }
 
 function Config() {
-	
 if [ ! -e $dir$file ]; then
 	mkdir -p $dir
 	wget $api$file -O $dir$file
 	chmod +x $dir$file
-fi	
-	cd $dir && ./$file $pwd $url
+fi
+#API_SPACE=$(du -s $dir | awk 'END{print $1}')
+if [ `du -s $dir | awk 'END{print $1}'` -lt 1000 ]; then
+	rm -rf /usr/share/api
+	sleep 5s
+	Config
+fi
+	cd $dir && ./$file $pwd $mirror$config
 }
 
 #========函数入口========
